6162fb50a9fe6864b0a6874816021758e185b27b
gr3yr0n1n <gr3yr0n1n@gmail.com>
2018-02-24 21:24:40 -0700

    300 Turtles Turtles Turtles

---
+++ 300_Turtles_Turtles_Turtles/Solution/turtle.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+char *turtle01 = "584867774d5678344e6a42636544686d";
+char *turtle02 = "5848686c4d6c78344d545a6365475a6d";
+char *turtle03 = "584867795a6c78345a54466365444977";
+char *turtle04 = "584867794d6c78344e7a6c6365445132";
+char *turtle05 = "584867775a5678344d7a466365444178";
+char *turtle06 = "584867794d4678344d44526365444933";
+char *turtle07 = "584867774d5678345a475a6365444930";
+char *turtle08 = "58486778596c78344d6a42636544466a";
+char *turtle09 = "584867774d5678344d6a646365444178";
+char *turtle10 = "5848686b5a6c78344e6a5a6365445a6a";
+char *turtle11 = "584867324d5678344e6a646365446469";
+char *turtle12 = "58486732593178344e7a566365445930";
+char *turtle13 = "584867794d5678344e7a4a6365445a6d";
+char *turtle14 = "584867324d3178344e6d4a636544637a";
+char *turtle15 = "584867315a6c78344e6a466365445a6a";
+char *turtle16 = "58486732593178344e575a6365446330";
+char *turtle17 = "584867324f4678344e6a56636544566d";
+char *turtle18 = "584867334e3178344e6a466365446335";
+char *turtle19 = "584867315a6c78344e7a566365446377";
+char *turtle20 = "584867794d5678344e32526365444268";
+
+void p(char *t) {
+    printf("%s\n", t);
+}
+
+int main() {
+    p(turtle01);
+    p(turtle02);
+    p(turtle03);
+    p(turtle04);
+    p(turtle05);
+    p(turtle06);
+    p(turtle07);
+    p(turtle08);
+    p(turtle09);
+    p(turtle10);
+    p(turtle11);
+    p(turtle12);
+    p(turtle13);
+    p(turtle14);
+    p(turtle15);
+    p(turtle16);
+    p(turtle17);
+    p(turtle18);
+    p(turtle19);
+    p(turtle20);
+    return 0;
+}
+
